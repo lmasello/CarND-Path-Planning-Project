d6676c642d847fe7bf431503fe4214f90b6691b0
lmasello <leandro.masello@motion-s.com>
2020-11-25 16:17:48 +0100

    Remove unused helper functions

--- src/helpers.h
+++ src/helpers.h
@@ -44,47 +44,5 @@ inline double distance(double x1, double y1, double x2, double y2) {
   return sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 }
 
-// // Calculate closest waypoint to current x, y position
-// int ClosestWaypoint(double x, double y, const vector<double> &maps_x,
-//                     const vector<double> &maps_y) {
-//   double closestLen = 100000; //large number
-//   int closestWaypoint = 0;
-//
-//   for (int i = 0; i < maps_x.size(); ++i) {
-//     double map_x = maps_x[i];
-//     double map_y = maps_y[i];
-//     double dist = distance(x,y,map_x,map_y);
-//     if (dist < closestLen) {
-//       closestLen = dist;
-//       closestWaypoint = i;
-//     }
-//   }
-//
-//   return closestWaypoint;
-// }
-//
-// // Returns next waypoint of the closest waypoint
-// int NextWaypoint(double x, double y, double theta, const vector<double> &maps_x,
-//                  const vector<double> &maps_y) {
-//   int closestWaypoint = ClosestWaypoint(x,y,maps_x,maps_y);
-//
-//   double map_x = maps_x[closestWaypoint];
-//   double map_y = maps_y[closestWaypoint];
-//
-//   double heading = atan2((map_y-y),(map_x-x));
-//
-//   double angle = fabs(theta-heading);
-//   angle = std::min(2*pi() - angle, angle);
-//
-//   if (angle > pi()/2) {
-//     ++closestWaypoint;
-//     if (closestWaypoint == maps_x.size()) {
-//       closestWaypoint = 0;
-//     }
-//   }
-//
-//   return closestWaypoint;
-// }
-
 
 #endif  // HELPERS_H
